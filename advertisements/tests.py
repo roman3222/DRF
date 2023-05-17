2e4e2b2d10b795461047f95ac5ab6eac7ec51fb8
roman3222 <romkakms3222@gmail.com>
2023-05-18 00:26:28 +1000

    add file

--- advertisements/tests.py
+++ advertisements/tests.py
@@ -4,7 +4,7 @@ from rest_framework.test import APIClient
 
 class TestSampleViews(TestCase):
     def test_views(self):
-        url = '/api/v1/adv/'
+        url = '/api/adv/'
         client = APIClient()
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
